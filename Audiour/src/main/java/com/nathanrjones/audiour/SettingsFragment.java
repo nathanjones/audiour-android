e70d2d46eb65130b5781bdfeb779962dd140bafa
Nathan Jones <me@nathanrjones.com>
2013-12-20 23:58:48 -0600

    Added useful links to the settings screen.

--- Audiour/src/main/java/com/nathanrjones/audiour/SettingsFragment.java
+++ Audiour/src/main/java/com/nathanrjones/audiour/SettingsFragment.java
@@ -1,7 +1,10 @@
 package com.nathanrjones.audiour;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.support.v4.app.ShareCompat;
 
 public class SettingsFragment extends PreferenceFragment {
 
@@ -12,6 +15,23 @@ public class SettingsFragment extends PreferenceFragment {
 
         // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.preferences);
+
+        Preference sharePref = findPreference("pref_share_app");
+
+        if (sharePref == null) return;
+
+        sharePref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                Intent shareIntent = ShareCompat.IntentBuilder.from(getActivity())
+                        .setType("text/plain")
+                        .setText(getString(R.string.app_store_url))
+                        .getIntent();
+
+                startActivity(shareIntent);
+                return true;
+            }
+        });
     }
 
 }
